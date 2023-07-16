eab9cfd567cfa366e395aa87fbeecfc3d66c4e38
Marcelo Nogueira <marcelocartoes.pa@gmail.com>
2023-07-16 16:20:35 -0300

    components

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,7 +1,11 @@
-import '@/styles/Home.module.scss'
+import '@/styles/home.module.scss'
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.scss'
+import Image from 'next/image'
+import logoImg from '@/public/logo.jpg'
+import { Input } from '../components/ui/input'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,8 +15,16 @@ export default function Home() {
     <Head>
       <title>ClickDesp - Faça seu Login</title>
     </Head>
-      <div className={styles.container}>
-        <h1>ClickDesp</h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt='logo ClickDesp' />
+        <div className={styles.login}>
+          <form>
+            <Input
+            placeholder='Digite seu Email'
+            />
+            
+          </form>
+        </div>
       </div>
     </>
   )

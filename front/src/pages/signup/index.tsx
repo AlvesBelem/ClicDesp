68fddedcb200838f7bd257e41382fbc23a9e5a20
Marcelo Nogueira <marcelocartoes.pa@gmail.com>
2023-07-16 18:14:12 -0300

    tela de login e cadastro

--- front/src/pages/signup/index.tsx
+++ front/src/pages/signup/index.tsx
@@ -4,39 +4,44 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.scss'
 import Image from 'next/image'
 import logoImg from '@/public/logo.jpg'
-import { Input } from '../components/ui/input'
-import { Button } from '../components/ui/button'
+import { Input } from '../../components/ui/input'
+import { Button } from '../../components/ui/button'
 import Link from 'next/link'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Signup() {
   return (
     <>
     <Head>
-      <title>ClickDesp - Faça seu Login</title>
+      <title>ClickDesp - Faça seu Cadastro</title>
     </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt='logo ClickDesp' />
         <div className={styles.login}>
           <form>
+            <h1>Criando sua Conta</h1>
+          <Input
+              placeholder='Digite seu Nome'
+              type='text'
+            />
             <Input
               placeholder='Digite seu Email'
               type='text'
             />
             <Input
-              placeholder='Digite sua senha'
+              placeholder='Digite sua Senha'
               type='password'
             />
             <Button 
             type="submit"
             loading={false}
             >
-              Acessar
+              Cadastrar
             </Button>
           </form>
-          <Link className={styles.text} href="/signup">Não possui uma conta? Cadastre-se...</Link>
+          <Link className={styles.text} href="/">Já possui uma conta? Faça o login...</Link>
         </div>
       </div>
     </>
